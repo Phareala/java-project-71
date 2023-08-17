aea5eeb06029e9f860446785c021c371ed033440
paul <pashaosadchii@mail.ru>
2023-08-17 16:03:34 +0300

    Checkstyle fix

--- app/src/main/java/hexlet/code/Builder.java
+++ app/src/main/java/hexlet/code/Builder.java
@@ -1,6 +1,13 @@
 package hexlet.code;
 
-import java.util.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.Arrays;
+import java.util.TreeSet;
+
 
 public class Builder {
 
