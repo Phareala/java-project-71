7728b4ac265eaf459e9d229bd26b6517c963c928
paul <pashaosadchii@mail.ru>
2023-09-28 14:25:23 +0300

    Edited parsing method

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,5 +1,8 @@
 package hexlet.code;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -9,8 +12,10 @@ public class Differ {
 
     public static String generate(String firstFilePath, String secondFilePath, String format) throws Exception {
 
-        Map<String, Object> firstFile = Parser.parser(firstFilePath, getExtension(firstFilePath));
-        Map<String, Object> secondFile = Parser.parser(secondFilePath, getExtension(secondFilePath));
+        String firstData = getData(firstFilePath);
+        String secondData = getData(secondFilePath);
+        Map<String, Object> firstFile = Parser.parser(firstData, getExtension(firstFilePath));
+        Map<String, Object> secondFile = Parser.parser(secondData, getExtension(secondFilePath));
         List<Map<String, Object>> diffList = new ArrayList<>(buildDiff(firstFile, secondFile));
         return Formatter.format(diffList, format);
     }
@@ -22,6 +27,14 @@ public class Differ {
     private static String getExtension(String filePath) {
         int index = filePath.lastIndexOf('.');
         return index > 0 ? filePath.substring(index + 1) : "";
+    }
+
+    public static String getData(String filePath) throws Exception {
+        Path path = Paths.get(filePath).toAbsolutePath().normalize();
+        if (Files.notExists(path) || Files.isDirectory(path)) {
+            throw new Exception("wrong file type or path");
+        }
+        return Files.readString(path);
     }
 
 }
--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -1,32 +1,29 @@
 package hexlet.code;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.HashMap;
 import java.util.Map;
 
 public class Parser {
-
-    public static HashMap jsonMap(Path path) throws IOException {
-        return new ObjectMapper().readValue(path.toFile(), HashMap.class);
-    }
-
-    public static HashMap ymlMap(Path path) throws IOException {
-        return new YAMLMapper().readValue(path.toFile(), HashMap.class);
-    }
-
-    public static Map<String, Object> parser(String pathFile, String fileFormat) throws IOException {
-        Path path = Paths.get(pathFile).toAbsolutePath().normalize();
+    public static Map<String, Object> parser(String content, String fileFormat) throws IOException {
         return switch (fileFormat) {
-            case "json" -> jsonMap(path);
-            case "yml", "yaml" -> ymlMap(path);
+            case "json" -> jsonMap(content);
+            case "yml", "yaml" -> ymlMap(content);
 
             default -> throw new IllegalStateException("Unexpected extension: " + fileFormat);
         };
     }
 
+    private static Map<String, Object> jsonMap(String content) throws IOException {
+        return new ObjectMapper().readValue(content, new TypeReference<>() {
+        });
+    }
+
+    private static Map<String, Object> ymlMap(String content) throws IOException {
+        return new YAMLMapper().readValue(content, new TypeReference<>() {
+        });
+    }
 }
